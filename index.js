d8b1a3c4da9e25811f6baa1c6cb8dcaaf3d10aac
Yahya <yahyahussein670@gmail.com>
2022-06-25 17:10:32 +0300

    retrieve the id name of the junk section and added an event listener, add an event listener to the junk category to help user select the preffered junk food

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ fetchfood();
 const getMeal = (meals) => {
 
     const mealCopy = [...meals.slice(0,4)]
-
+    console.log(mealCopy)
 
     mealCopy.forEach(meal => {
     let card = document.createElement("li")
@@ -29,21 +29,69 @@ const getMeal = (meals) => {
     });
 }
 
+
+
 const getCategories = (categories) => {
+
+
     const copyCategories = [...categories.slice(0, 2)]
     copyCategories.forEach(category => {
+
+        let cut = category.ingredients.split('.').join('\n')
+        console.log(cut);
+
+       
+
         let products = document.createElement("li")
         products.className = "products" 
-        
+
+         let render = document.getElementById("five")
+        render.addEventListener("click", () => {
+            let mainfile = document.querySelector(".mainfile")
+
+            mainfile.style.display = "none"
+
+            let junkfile = document.querySelector(".mainjunk")
+            junkfile.style.display = "initial"
+
         products.style.listStyle = "none"
 
         products.innerHTML = `
         <img src = "${category.image}">
-        <div class = "content"> 
+        <div class = "content1"> 
         <h4>${category.name}</h4>
         <br>
         <p>${category.duration }mins</p>`
 
         document.querySelector(".redirect").appendChild(products)
+    })
+
+        products.addEventListener("click", (e) => {
+            e.preventDefault()
+            let fullPage = document.querySelector(".mainjunk");
+            fullPage.style.display = "none"
+
+            let junkItem = document.createElement("div")
+
+            junkItem.className = "junkitem"
+
+
+            junkItem.innerHTML = `
+            <img src = "${category.image}">
+            <div class = "content2">
+            <h2>${category.name}</h2>
+            <br>
+            <h3>Description</h3>
+            <p>${category.description}</p>
+            <br>
+            <h3>Methods</h3>
+            <p>${cut}</p>
+            <br>
+            <h3>Cooking Duration</h3>
+            <p>${category.duration} mins <p>
+            `
+            document.querySelector(".junk").append(junkItem)
+
+        })
     });   
 }
